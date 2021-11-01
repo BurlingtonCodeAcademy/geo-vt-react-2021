23ade0d2c421c0fadd3ec15c2420eda17acf16fc
Bob Stauss <rstauss@burlingtoncodeacademy>
2021-11-01 13:28:31 -0400

    prebuilt leaflet-pip function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import './App.css';
 import { useState } from 'react'
+import L from 'leaflet'
+import borderData from './data/border';
+import leafletPip from 'leaflet-pip'
 
 import Map from './components/Map'
+
+//Takes two arguments (latitude, and longitude)
+//returns true if those coordinates are inside VT and false if they are outside
+function inVermont(lat, long) {
+  let vtLayer = L.geoJSON(borderData)
+
+  let layerArray = leafletPip.pointInLayer([long, lat], vtLayer)
+
+  if(layerArray.length) {
+    return true
+  } else {
+    return false
+  }
+}
 
 function App() {
 
